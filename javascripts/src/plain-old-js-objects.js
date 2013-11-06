7da970f7ec9907f21c675761a94b4efee7127cc3
James Bond <james@devbootcamp.com>
2013-11-05 17:08:13 -0800

    implemented code to pass all but last 2 specs

--- javascripts/src/plain-old-js-objects.js
+++ javascripts/src/plain-old-js-objects.js
@@ -1,1 +1,23 @@
+FRUIT_BEARING_AGE = 5
+MAX_AGE = 75
+
 /* Write your code for the plain-old-js-objects-spec here */
+function createTree() {
+  var tree = {
+    age: 0,
+    height: 0,
+    orangeCount: 0,
+    isAlive: true,
+    grow : function() {
+      this.age += 1
+      this.height += 10
+      if (this.age >= FRUIT_BEARING_AGE) {
+        this.orangeCount += Math.floor((Math.random() * 10) + 1)
+      }
+      if (this.age > MAX_AGE) {
+        this.isAlive = false
+      }
+    }
+  }
+  return tree
+}
