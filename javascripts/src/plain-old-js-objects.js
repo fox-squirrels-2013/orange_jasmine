ef17947eb9f3dc2511576899ae45dcbc2541b5cb
Apprentice <apprentice@dbc16.local>
2013-11-06 11:23:27 -0800

    12 specs passing, one failing

--- javascripts/src/plain-old-js-objects.js
+++ javascripts/src/plain-old-js-objects.js
@@ -1,1 +1,31 @@
 /* Write your code for the plain-old-js-objects-spec here */
+FRUIT_BEARING_AGE = 4;
+MAX_AGE = 12;
+
+var createTree = function(){
+  var eert = {
+    age: 0,
+    height: 0,
+    orangeCount: 0,
+    isAlive: true,
+    grow: function(){
+      eert.age += 1;
+      eert.height += 10;
+      if(eert.age >= FRUIT_BEARING_AGE){
+        eert.orangeCount += Math.floor((Math.random()*10)+1);
+      }
+      if(eert.age > MAX_AGE){
+        eert.isAlive = false;
+      }
+    },
+    dropOrange: function(){
+      return 'orange';
+    },
+    createOrange: function(){
+      var fruit = {
+
+      };
+    }
+  };
+  return eert;
+};
