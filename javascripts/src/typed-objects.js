210e85ef0332fbe2f741cd3ef18873821047a241
Nicole Paz <nicole@Nicoles-MacBook-Pro.local>
2013-11-06 19:49:37 -0800

    Created and passed constructor tests

--- javascripts/src/typed-objects.js
+++ javascripts/src/typed-objects.js
@@ -1,3 +1,37 @@
-// Write your Orange Tree code here - you may use constructor functions
+function Tree() {
+  FRUIT_BEARING_AGE = 10
+  MAX_AGE = 30
 
-// var Tree = new Object();
+  this.age = 0
+  this.height = 0
+  this.orangeCount = 0
+  this.isAlive = true
+
+  this.dropOrange = function() {
+    if (this.age >= FRUIT_BEARING_AGE) {
+      this.orangeCount -= 1
+      return 1
+    }
+  }
+
+  this.grow = function() {
+    this.age += 1
+    this.height += 10
+
+    if (this.age >= FRUIT_BEARING_AGE) {
+      this.orangeCount = Math.floor(Math.random() * 10 + 1)
+    }
+
+    if (this.age > MAX_AGE) {
+      this.isAlive = false
+    }
+  }
+}
+
+function CreateOrange() {
+  var orange = new Tree()
+
+  orange.diameter = Math.floor(Math.random() * 10 + 1)
+
+  return orange
+}
